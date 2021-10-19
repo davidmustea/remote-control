7319a0e1a9e2e5cf471aab5f7373d7c26eb3cfab
Mustea David <puddinglord@Puddings-Mac-mini.local>
2021-10-19 22:53:02 +0300

    gata fara functii speciale

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
+import subprocess
+import time
+import pyautogui
+
+app = Flask(__name__)
+
+@app.route("/cmd", methods=["GET", "POST"])
+def cmd():
+    if request.method == "GET":
+        return render_template("cmd.html")
+    if request.method == "POST":
+        data_form_comenzi = str(request.form.get("form_comenzi"))
+        comanda = subprocess.run(data_form_comenzi, capture_output=True, text=True, shell=True)
+        return render_template("cmd.html", comanda=comanda.stdout)
+
+@app.route("/type", methods=["GET", "POST"])
+def type():
+    if request.method == "GET":
+        return render_template("type.html")
+    if request.method == "POST":
+        data_form_type = str(request.form.get("form_type"))
+        for letter in data_form_type:
+            pyautogui.typewrite(letter)
+            time.sleep(0.1)
+        return render_template("type.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000, host="192.168.0.213")
